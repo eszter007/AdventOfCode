100b8e154c438a2a06446020835b2c7408f52675
Eszter Schuffert <eszter@Eszters-iMac.fritz.box>
2020-12-01 20:58:58 +0100

    comment

--- 01/main.py
+++ 01/main.py
@@ -3,6 +3,7 @@ import itertools
 
 def calculate2020(file):
 	data = [int(line.strip()) for line in open(file, 'r')]
+	# for task 1, z was removed
 	for x,y,z in itertools.combinations(data, 3):
 		if x + y + z == 2020:
 			return x*y*z
