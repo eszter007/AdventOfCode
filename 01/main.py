24b6510ac1d5ddb183a98401a48da1319e490dc1
Eszter Schuffert <eszter@Eszters-iMac.fritz.box>
2020-12-01 20:35:47 +0100

    first day

---
+++ 01/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import itertools
+
+def calculate2020(file):
+	data = [int(line.strip()) for line in open(file, 'r')]
+	for x,y,z in itertools.combinations(data, 3):
+		if x + y + z == 2020:
+			return x*y*z
+			break
+
+print(calculate2020("input.txt"))
