cf061c103926393284ca03b93b4df0a7af33b3da
Eszter Schuffert <schufferte@yahoo.com>
2020-12-02 12:11:34 +0100

    use dictionary

--- 02/main.py
+++ 02/main.py
@@ -3,30 +3,43 @@ file = "input.txt"
 
 data = [line.strip() for line in open(file, 'r')]
 
-def task1():
-	counter = 0
+def convertToDicts():
+	policies = [dict()]
 	for line in data:
 		subset = line.split(" ")
 		times = tuple(map(int, subset[0].split("-")))
 		letterToBeContained = subset[1][0]
 		password = subset[2]
-		occurencesOfLetter = password.count(letterToBeContained)
-		if occurencesOfLetter >= times[0] and occurencesOfLetter <= times[1]:
-			counter += 1
-	print(counter)
+		
+		passwordPolicy = {
+			"policyTuple": times, 
+			"letter": letterToBeContained,
+			"password": password,
+		}
+		
+		policies.append(passwordPolicy)
+	return policies
+
+def task1():
+	counter = 0
+	for value in convertToDicts():
+		if bool(value):
+			occurences = value["password"].count(value["letter"])
+			if value["policyTuple"][0] <= value["password"].count(value["letter"]) <= value["policyTuple"][1]:
+				counter += 1
+	return counter
 
 def task2():
 	counter = 0
-	for line in data:
-		subset = line.split(" ")
-		times = tuple(map(int, subset[0].split("-")))
-		letterToBeContained = subset[1][0]
-		password = subset[2]
-		validator = False
-		if password[times[0]-1] == letterToBeContained and password[times[1]-1] != letterToBeContained: validator = True
-		elif password[times[0]-1] != letterToBeContained and password[times[1]-1] == letterToBeContained: validator = True
-		if validator: counter += 1
-	print(counter)
+	for value in convertToDicts():
+		if bool(value):
+			validator = False
+			first = value["password"][value["policyTuple"][0]-1]
+			second = value["password"][value["policyTuple"][1]-1]
+			if (first == value["letter"] and second != value["letter"]) \
+				or (first != value["letter"] and second == value["letter"]): validator = True
+			if validator: counter += 1
+	return counter
 
-task1()
-task2()
+print("Solution 1: " + str(task1()))
+print("Solution 2: " + str(task2()))
