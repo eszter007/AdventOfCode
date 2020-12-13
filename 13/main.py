8e2854e88a8c632c7cfa9247fc4450576f5238a0
Eszter Schuffert <schufferte@yahoo.com>
2020-12-13 22:37:41 +0100

    Day 13 - Part 1

---
+++ 13/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+file = "input.txt"
+data = [line.strip() for line in open(file, 'r')]
+
+departure = int(data[0])
+busses = data[1].split(",")
+busNos = []
+for bus in busses:
+	if bus != "x":
+		busNos.append(int(bus))
+
+def part1():
+	mod = 0
+	earliestBus = 0
+	for bus in busNos:
+		diff = departure % bus
+		if (bus - diff) < mod or mod == 0:
+			mod = bus - diff
+			earliestBus = bus
+	print("Part 1: " + str(earliestBus*mod))
+part1()
