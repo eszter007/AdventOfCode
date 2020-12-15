4aa34c7d21d734a3154b7ccb18d7f4b1593e2b44
Eszter Schuffert <schufferte@yahoo.com>
2020-12-15 16:56:31 +0100

    Day 15

---
+++ 15/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+file = "input.txt"
+#data = [(9, False), (19, False), (1, False), (6, False), (0, False) (5, False), (4, False)]
+data = [0, 3, 6
+
+
+print([item for item in data if item[0] == 0])
