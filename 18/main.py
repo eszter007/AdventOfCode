6dc0c00ef0deadcfb189e99c924fb9bfeeed6667
Eszter Schuffert <schufferte@yahoo.com>
2020-12-18 23:31:48 +0100

    Day 18 - Part 2

--- 18/main.py
+++ 18/main.py
@@ -8,15 +8,15 @@ data = [line.strip() for line in open(file, 'r')]
 # Part 1
 #
 
-def run(calc):
+def run(calc, name):
 	match = re.search(r'\(([0-9]|\*|\+|\s)*\)', calc)
 	if match:
 		subExpression = match.group()[1:-1].strip()
 		subRange = match.span()
 		before = calc[0:subRange[0]]
 		after = calc[subRange[1]:]
-		return run(before + str(calculate(subExpression)) + after)
-	return calculate(calc)
+		return run(before + str(name(subExpression)) + after, name)
+	return name(calc)
 
 def calculate(calc):
 	calc = calc.split()
@@ -28,11 +28,29 @@ def calculate(calc):
 		else: num = eval(str(num) + symbol + char)
 	return num
 
-def part1():
+def parts(name):
 	solutions = []
 	for line in data:
-		solutions.append(run(line))
-	
-	print("Part 1: " + str(sum(solutions)))
+		solutions.append(run(line, name))
+	return str(sum(solutions))
 
-part1()
+#
+# Part 2
+#
+
+def calcAddition(calc):
+	match = re.search(r'[0-9]+\s\+\s[0-9]+', calc)
+	if match:
+		subExpression = match.group()
+		subRange = match.span()
+		before = calc[0:subRange[0]]
+		after = calc[subRange[1]:]
+		return calcAddition(before + str(eval(subExpression)) + after)
+	else: return eval(calc)
+
+#
+# Solutions
+#
+
+print("Part 1: " + parts(calculate))
+print("Part 2: " + parts(calcAddition))
